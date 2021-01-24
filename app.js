6b12333917d82d2f0af3593c25606dd411708db1
CryptoAlgo-Dev <64193267+cryptoAlgorithm@users.noreply.github.com>
2021-01-24 15:06:07 +0800

    Updates separation game data and answers now

--- app.js
+++ app.js
@@ -85,7 +85,9 @@ const main = () => {
             nestedSubTopics: {},
             nestedSubTopicsContent: {},
             pHArray: {},
-            gameTopics: {}
+            gameTopics: {},
+            gasGameArray: snapshot.child('gasesArray').val(),
+            sepGameArray: {}
         };
         let index = 0
         snapshot.child('learnTopics').forEach((learnTopic) => {
@@ -177,13 +179,71 @@ const main = () => {
         iter = 0; // Reuse same var
         // Game topics
         snapshot.child('gameTopics').forEach((gameTopic) => {
+            let gameDesc;
+            let icon;
+            switch (iter) {
+                case 0:
+                    gameDesc = 'Your cat just crash-landed on Earth from the Moon and all the gas tanks in its rocket got mixed up.\n' +
+                        '\n' +
+                        'As your cat can only live in a certain type of gas, your task is to use your knowledge of gas tests to help it identify which gas tank to attach to its holding room so that it can survive.';
+                    icon = 'gas_tank';
+                    break;
+                case 1:
+                    gameDesc = 'Your cat encountered a wizard.\n' +
+                        'Separate all the elements in his beaker or the wizard will cast a spell on your cat.\n' +
+                        'A new element is added for every wrong method chosen, so do not let it overflow!';
+                    icon = 'wizard';
+                    break;
+                case 2:
+                    gameDesc = 'Your cat has met a monster who can only live in either acidic or alkali conditions.\n' +
+                        '\n' +
+                        'The monster is currently living in the right conditions, and your task is to use the indicators and solutions provided to find out what that condition is and how to kill the monster to save your cat.';
+                    icon = 'cat_happy';
+                    break;
+                case 3:
+                    gameDesc = 'Your cat is starving after being on Earth for so long. However, it cannot digest regular Earth food. Its diet only consists of the first 18 elements of the periodic table.\n' +
+                        '\n' +
+                        'Given the properties of each Group of the periodic table, select the correct group, or the alien cat will die!';
+                    icon = 'bowl';
+                    break;
+                default:
+                    gameDesc = 'No description available';
+            }
+
             outBuff.gameTopics[iter.toString()] = {
                 icon: iOStoAndroidImg(gameTopic.child('pic').val()),
                 title: gameTopic.child('title').val(),
-                unlockPoints: gameTopic.child('pointsNeeded').val()
+                unlockPoints: gameTopic.child('pointsNeeded').val(),
+                description: gameDesc,
+                gameIcon: icon
             }
             iter++;
         });
+
+        let solidSepArray = [];
+        const BreakException = {};
+        iter = 0; // Reuse again
+        // Separation game
+        snapshot.child('retrieveSolidsArray').forEach((solidArray) => {
+            // Find correct answer
+            let correctAnsIndex = 0;
+            try {
+                solidArray.child('givenMethods').child('methods').forEach((method) => {
+                    if (method.child('isCorrect').val()) throw BreakException; // Not so pretty way of exiting from a forEach loop
+                    correctAnsIndex++;
+                });
+            } catch (err) {
+                if (err !== BreakException) throw err;
+            }
+
+            solidSepArray[iter] = {
+                title: solidArray.child('name').val().charAt(0).toUpperCase() + solidArray.child('name').val().slice(1)
+                    + ' - ' + solidArray.child('properties').val().charAt(0).toUpperCase() + solidArray.child('properties').val().slice(1),
+                correctAns: correctAnsIndex
+            }
+            iter++;
+        });
+        outBuff.sepGameArray = solidSepArray;
 
         console.log('\nData to be written to output database:\n', outBuff);
         // Write the data to the output database
